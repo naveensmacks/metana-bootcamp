d858d733219add2e60ca63597a67398a2372bf9d
naveensmacks <naveen.ilearn@gmail.com>
2023-05-20 23:58:18 +0300

    Coded without Assembly code

--- module-10/contracts/String.sol
+++ module-10/contracts/String.sol
@@ -2,23 +2,17 @@
 pragma solidity ^0.8.4;
 
 contract String {
-    function charAt(
-        string memory input,
-        uint index
-    ) public pure returns (bytes2) {
-        bytes2 result = 0x0000;
-        assembly {
-            if lt(index, mload(input)) {
-                result := shl(8, mload(add(add(input, 0x20), index)))
-            }
+    function charAt(string memory input, uint index) public pure returns (bytes2) {
+        bytes memory inputBytes = bytes(input);
+        if (index >= inputBytes.length) {
+            return 0x0000;
+        } else {
+            return bytes2(inputBytes[index]);
         }
-        return result;
     }
 }
 
-
-
-// Add following test cases for String contract: 
+// Add following test cases for String contract:
 // charAt(“abcdef”, 2) should return 0x6300
 // charAt(“”, 0) should return 0x0000
 // charAt(“george”, 10) should return 0x0000
