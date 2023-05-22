d9119bdbc3f62c7268a7b6599d3087b07385b407
naveensmacks <naveen.ilearn@gmail.com>
2023-05-22 23:20:40 +0300

    Initial code of Assembly code for charAt function

--- module-10/contracts/String.sol
+++ module-10/contracts/String.sol
@@ -10,9 +10,27 @@ contract String {
             return bytes2(inputBytes[index]);
         }
     }
+
+    // using inline assembly code
+    function charAtAsm(string memory input, uint index) public pure returns (uint, uint, bytes32, bytes1) {
+        uint length;
+        uint dataStart;
+        bytes32 byteVal;
+        uint8 char;
+        assembly {
+            length := mload(input)
+            dataStart := add(input, 0x20)
+            if lt(index, length) {
+                let charPos := add(dataStart, index)
+                byteVal := mload(charPos)
+                char := byte(0, byteVal) // load the rightmost byte only
+            }
+        }
+        return (length, dataStart, byteVal, bytes1(char));
+    }
 }
 
-// Add following test cases for String contract:
-// charAt(“abcdef”, 2) should return 0x6300
-// charAt(“”, 0) should return 0x0000
-// charAt(“george”, 10) should return 0x0000
+// Add following test cases for String contract: 
+// charAt("abcdef", 2) should return 0x6300
+// charAt("", 0) should return 0x0000
+// charAt("george", 10) should return 0x0000
