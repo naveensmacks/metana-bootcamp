a72437bd3a91a5c0cb3fdf1ea5eef8784bcdd7a3
naveensmacks <naveen.ilearn@gmail.com>
2023-08-08 23:57:26 +0530

    Generate hash of the message

--- module-11/walletProject/wallet-project/src/components/WalletGenerator.js
+++ module-11/walletProject/wallet-project/src/components/WalletGenerator.js
@@ -5,6 +5,10 @@ const WalletGenerator = () => {
   const [address, setAddress] = useState('');
   const [privateKey, setPrivateKey] = useState('');
 
+  const [nonce, setNonce] = useState('');
+  const [textboxContent, setTextboxContent] = useState('');
+  const [hashedMessage, setHashedMessage] = useState('');
+
   const createNewAccount = () => {
     // Step 1: Generate a random private key
     const privateKeyBytes = ethers.utils.randomBytes(32);
@@ -22,6 +26,31 @@ const WalletGenerator = () => {
     // Update the state with the new address and private key
     setAddress(newAddress);
     setPrivateKey(newPrivateKey);
+
+
+    // Step 3: Generate a nonce (can be a simple counter or timestamp)
+    const newNonce = Math.floor(Math.random() * 100000); // Replace with appropriate nonce generation logic
+
+    // Update the state with the new nonce
+    setNonce(newNonce);
+  };
+
+   //Calculate hash
+   const calculateHash = (content, nonce) => {
+    //HASH MESSAGE + CURRENT NONCE TO PREVENT REPLAY ATTACK
+    //const messageToHash = content + nonce.toString();
+    //const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(messageToHash));
+    const hash = ethers.utils.solidityKeccak256(["string", "uint256"], [content, nonce]);
+    return hash;
+  };
+
+
+  //Update hash when textbox content changes
+  const handleTextboxChange = (event) => {
+    const newTextboxContent = event.target.value;
+    setTextboxContent(newTextboxContent);
+    const hash = calculateHash(newTextboxContent, nonce);
+    setHashedMessage(hash);
   };
 
   return (
@@ -29,6 +58,14 @@ const WalletGenerator = () => {
       <button onClick={createNewAccount}>Create a New Account</button>
       {address && <p>Ethereum Address: {address}</p>}
       {privateKey && <p>Private Key: {privateKey}</p>}
+
+      {nonce && (
+        <div>
+          <label>Hash the message + current Nonce : </label>
+          <input type="text" value={textboxContent} onChange={handleTextboxChange}  placeholder={"Insert your message here"} />
+          <p>Hashed Message: {hashedMessage}</p>
+        </div>
+      )}
     </div>
   );
 };
