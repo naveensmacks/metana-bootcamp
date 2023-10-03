4655e54327145a384ed19cb21a8592cc966e4edc
naveensmacks <naveen.ilearn@gmail.com>
2023-10-03 22:39:49 +0530

    Uniswap swap tokens for tokens

---
+++ module-12/UniSwap/contracts/interfaces/Uniswap.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+// https://uniswap.org/docs/v2/smart-contracts
+
+interface IUniswapV2Router {
+
+  function swapExactTokensForTokens(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+
+}
