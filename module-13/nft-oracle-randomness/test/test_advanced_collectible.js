b4f0b20b3cba581266f5e85f9b3029b93252cc0b
naveensmacks <naveen.ilearn@gmail.com>
2024-01-11 15:44:20 +0530

    added event listener and Timeout for return response

--- module-13/nft-oracle-randomness/test/test_advanced_collectible.js
+++ module-13/nft-oracle-randomness/test/test_advanced_collectible.js
@@ -14,31 +14,40 @@ describe("AdvancedCollectible", function () {
     });
 
     it("Should create an advanced collectible", async function () {
-
-        //dai = await ethers.getContractAt("IERC20", DAI);
-        //event ReturnedCollectible(uint256 indexed newItemId, Breed breed, uint256 randomNumber);
-        // Begin listening for any Transfer event
-        advancedCollectible.on("ReturnedCollectible", (newItemId, breed, randomNumber, event) => {
+        //setting timeouit for mocha test, the test exceeding Mocha's default timeout limit.(2 or 4secs)
+        //since we're waiting for an external response from the Chainlink VRF coordinator, the operation takes longer than the default timeout.
+        this.timeout(200000); 
+        //listen to emit event  event ReturnedCollectible(uint256 indexed newItemId, Breed breed, uint256 randomNumber);
+        advancedCollectible.on("ReturnedCollectible", async (newItemId, breed, randomNumber, event) => {
+            console.log("event");
             console.log(`newItemId: ${ newItemId } , breed Number: ${ breed }, randomNumber from vrf: ${ randomNumber }`);
             
             // The `event.log` has the entire EventLog
             console.log("Event.log : ", event.log.args);
-  
+            
+            const tokenCounter = await advancedCollectible.tokenCounter();
+            console.log("tc: ", tokenCounter);
+            // Assert - check if the token counter is incremented
+            expect(tokenCounter).to.be.above(0);
             // Optionally, stop listening
           });
         // Act - request to create a collectible
-        const createTx = await advancedCollectible.createCollectible("None");
+        console.log(3)
+        const createTx = await advancedCollectible.createCollectible("None 2");
+        console.log(4)
         await createTx.wait();
-
+       
         // Wait for the VRF Coordinator to respond
         // This may require a significant wait time depending on network conditions
-        // Optionally, listen for a specific event if your contract emits one after VRF fulfillment
-
-        // Assert - check if the token counter is incremented
-        const tokenCounter = await advancedCollectible.tokenCounter();
-        expect(tokenCounter).to.be.above(0);
-
-        //listen to emit event  event ReturnedCollectible(uint256 indexed newItemId, Breed breed, uint256 randomNumber);
+        await new Promise(res => setTimeout(res, 150000));
 
     });
+
+    it.skip("check tokenCounter after createCollectible", async function () {
+        const tokenCounter = await advancedCollectible.tokenCounter();
+        console.log("tc: ", tokenCounter);
+        // Assert - check if the token counter is incremented
+        expect(tokenCounter).to.be.above(0);
+    });
+
 });
